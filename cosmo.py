c2947a5252ebb961e20b53daded6e2d7412a262f
Ronjhon10 <39675059+Ronjhon10@users.noreply.github.com>
2025-11-24 12:33:49 -0700

    Error Stringing

--- cosmo.py
+++ cosmo.py
@@ -21,6 +21,7 @@ class Error:
     def as_string(self):
         result = f'{self.error_name}: {self.details}'
         result += f'\nFile {self.position_start.fn}, line {self.position_start.ln + 1}'
+        result += '\n\n'+string_with_arrows(self.position_start.ftxt, self.position_start, self.position_end)
         return result
 class IllegalCharError(Error):
     def __init__(self, position_start, position_end, details):
