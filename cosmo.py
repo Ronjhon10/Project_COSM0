efae073d9d901e306e6cfb83e8f98e7c31b9c943
Ronjhon10 <39675059+Ronjhon10@users.noreply.github.com>
2025-11-24 12:34:51 -0700

    type_ fix

--- cosmo.py
+++ cosmo.py
@@ -68,7 +68,7 @@ TT_RPAREN= 'RPAREN'
 
 
 class Token:
-    def __init__(self, type_, value = None):
+    def __init__(self, type_, value=None):
         self.type_ = type_
         self.value = value
     def __repr__(self):
