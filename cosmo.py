9b81397c4338beb1866d4bd64c970e011e8246d1
Ronjhon10 <39675059+Ronjhon10@users.noreply.github.com>
2025-11-26 10:38:54 -0700

    Defined runtime errors. Focus on division by zero.

--- cosmo.py
+++ cosmo.py
@@ -29,6 +29,27 @@ class IllegalCharError(Error):
 class InvalidSyntaxError(Error):
     def __init__(self, position_start, position_end, details=""):
         super().__init__(position_start, position_end, 'Invalid Syntax', details)
+class RTError(Error):
+    def __init__(self, position_start, position_end, details, context):
+        super().__init__(position_start, position_end, 'Runtime Error', details)
+        self.context = context
+
+    def as_string(self):
+        result = self.generate_traceback()
+        result += f'{self.error_name}: {self.details}'
+        result += '\n\n' + string_with_arrows(self.position_start.ftxt, self.position_start, self.position_end)
+        return result
+
+    def generate_traceback(self):
+        result = ''
+        pos =self.position_start
+        ctx = self.context
+
+        while ctx:
+            result = f'File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
+            pos = ctx.parent_entry_pos
+            ctx = ctx.parent
+        return 'Traceback (most recent call last):\n' + result
 
 # <><><><><><><><><><><><><><><><><><><><><>
 # POSITION
